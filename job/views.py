b9ec61afd1006da05f49a2ff1c1dc340307995c0
shadowz-dev <zwildzshadow@gmail.com>
2020-06-07 16:06:15 +0200

    created simple view model for jobs

--- job/views.py
+++ job/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
+from .models import Job
 
 # Create your views here.
 
 def job_list(request):
-    pass
+    job_list = Job.objects.all() # return all jobs inside jobs model db
+    context = {'jobs' :job_list } # Template Name in string
+    return render(request,'job/job_list.html', context) # render the request in the html with the context
 
 
 
 
 
 def job_details(request , id):
-    pass
+    job_details = Job.objects.get(id=id)
+    context = {'job' :job_details}
+    return render(request,'job/job_details.html',context)
